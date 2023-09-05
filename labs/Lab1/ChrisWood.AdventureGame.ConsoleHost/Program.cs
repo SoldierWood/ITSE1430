e8089898f3b894e44d7692a738507215d7b9f0ab
Chris Wood <chris.wood688@my.tccd.edu>
2023-09-05 15:45:26 -0500

    3:45 Changes

--- labs/Lab1/ChrisWood.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/ChrisWood.AdventureGame.ConsoleHost/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lab 1
+// Written by Chris Wood
+// Fall 2023
+
+
+DisplayIntro();
+
+
+void DisplayIntro ()
+{
+
+    Console.WriteLine("ITSE 1430 Adventure Game");
+    Console.WriteLine("Written by Chris Wood");
+    Console.WriteLine("Fall 2023");
+
+}
+
