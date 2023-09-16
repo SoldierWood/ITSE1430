75fabb9a88621a556fc727285ad8767c767fd1da
Chris Wood <chris.wood688@my.tccd.edu>
2023-09-15 20:30:15 -0500

    09 15 23 20:30

--- labs/Lab1/ChrisWood.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/ChrisWood.AdventureGame.ConsoleHost/Program.cs
@@ -5,7 +5,6 @@
 
 // Entry point
 int currentRoom = 1;
-//int newRoom = 0;
 
 DisplayIntro();
 
@@ -111,9 +110,29 @@ int GetUserSelection ()
 
 int MoveNorth ( int currentRoom )
 {
-    if (currentRoom == 1)
+    if ((currentRoom == 1) || (currentRoom==2) || (currentRoom==3))
     {
-        Console.WriteLine("You cannot move North from the gas station. Please try a different direction.\n");
+        Console.WriteLine("You cannot move North from this area. Please try a different direction.\n");
+    } else if (currentRoom == 4)
+    {
+        currentRoom = 1;
+        room1Scenario();
+    } else if (currentRoom ==5)
+    {
+        currentRoom = 2;
+        room2Scenario();
+    } else if (currentRoom == 6)
+    {
+        currentRoom = 3;
+        room3Scenario();
+    } else if (currentRoom == 7)
+    {
+        currentRoom = 4;
+        room4Scenario();
+    } else if (currentRoom == 8)
+    {
+        currentRoom = 5;
+        room5Scenario();
     }
 
     return currentRoom;
@@ -121,40 +140,122 @@ int MoveNorth ( int currentRoom )
 
 int MoveSouth ( int currentRoom )
 {
-
+    if (currentRoom == 1)
+    {
+        currentRoom = 4;
+        room4Scenario();
+    } else if (currentRoom == 2)
+    {
+        currentRoom = 5;
+        room5Scenario();
+    } else if (currentRoom == 3)
+    {
+        currentRoom = 6;
+        room6Scenario();
+    } else if (currentRoom == 4)
+    {
+        currentRoom = 7;
+        room7Scenario();
+    } else if (currentRoom == 5)
+    {
+        currentRoom = 8;
+        room8Scenario();
+    } else if (currentRoom == 6)
+    {
+        currentRoom = 9;
+        room9Scenario();
+    } else if ((currentRoom==7) || (currentRoom==8) || (currentRoom==9))
+    {
+        Console.WriteLine("You cannot move South from this area. Please try a different direction.\n");
+    }
     return currentRoom;
 }
 
 int MoveEast (int currentRoom)
 {
-    //int roomNew = currentRoom;
-
     if (currentRoom == 1)
     {
         currentRoom = 2;
         room2Scenario();
      }
-    
     else if (currentRoom == 2)
     {
         currentRoom = 3;
         room3Scenario();
     }
-
-    return currentRoom;
-
-}
-
-int MoveWest (int currentRoom)
-{
-    if (currentRoom == 1)
+    else if (currentRoom == 4)
     {
-        Console.WriteLine("You cannot move West from the gas station. Please try a different direction.\n");
+        currentRoom = 5;
+        room5Scenario();
+    }
+    else if (currentRoom == 5)
+    {
+        currentRoom = 6;
+        room6Scenario();
+    }
+    else if (currentRoom == 7)
+    {
+        currentRoom = 8;
+        room8Scenario();
+    } else if (currentRoom == 8)
+    {
+        currentRoom = 9;
+        room9Scenario();
+    }
+    else if ((currentRoom==3) || (currentRoom==6) || (currentRoom==9))
+    {
+        Console.WriteLine("You cannot move East from this area. Please try a different direction.\n");
     }
 
     return currentRoom;
 }
 
+int MoveWest (int currentRoom)
+{
+    if (currentRoom == 2)
+    {
+        currentRoom = 1;
+        room1Scenario();
+    } 
+    else if (currentRoom == 5)
+    {
+        currentRoom = 4;
+        room4Scenario();
+    } 
+    else if (currentRoom == 8)
+    {
+        currentRoom = 7;
+        room7Scenario();
+    } 
+    else if (currentRoom == 3)
+    {
+        currentRoom = 2;
+        room2Scenario();
+    } 
+    else if (currentRoom == 6)
+    {
+        currentRoom = 5;
+        room5Scenario();
+    } 
+    else if (currentRoom == 9)
+    {
+        currentRoom = 8;
+        room8Scenario();
+    }
+    else if ((currentRoom == 1) || (currentRoom == 4) || (currentRoom == 7))
+    {
+        Console.WriteLine("You cannot move West from this area. Please try a different direction.\n");
+    }
+
+    return currentRoom;
+}
+
+void room1Scenario()
+{
+    Console.WriteLine("This is the run-down gas station you were abandoned at. It is in desperate need of repair");
+    Console.WriteLine("Illegal items are sold in this store, so the clerk is afraid of calling the police unless ");
+    Console.WriteLine("it is a dire emergency.\n");
+}
 void room2Scenario ()
 {
     Console.WriteLine("You have pulled in front of a run down looking house. The old man acompanying you wants you to go ");
@@ -168,5 +269,51 @@ void room3Scenario()
     Console.WriteLine("The old man asks you to see if one of the women will get him more pain medicine.");
     Console.WriteLine("With great trepidation, you approach the women and make your request.");
     Console.WriteLine("One of the women agrees to help and takes the money. After what seems like a long time,");
-    Console.WriteLine("the woman returns with some medicine to give to the old man.");
+    Console.WriteLine("the woman returns with some medicine to give to the old man.\n");
+}
+
+void room4Scenario()
+{
+    Console.WriteLine("You turn into a dark alley. There is a long, wooden fence along one side. ");
+    Console.WriteLine("A man's head pops up over the fence. The old man asks you to stop so he can say ");
+    Console.WriteLine("hello to an old friend. The old man rolls down his window, they talk for a few minutes, ");
+    Console.WriteLine("then the old man tells you to move on.\n");
+}
+
+void room5Scenario()
+{
+    Console.WriteLine("You arrive in front of a liquor store. The old man gives you 3 one-dollar bills and some change. ");
+    Console.WriteLine("You tell him that he has not given you enough money. The old man educates you that this liquor store ");
+    Console.WriteLine("sells small bottles of liquor for $1 each. He would like three small bottle of liquor. You go in ");
+    Console.WriteLine("and purchase the alcohol on the old man's behalf.\n");
+}
+void room6Scenario()
+{
+    Console.WriteLine("You drive up in front of a small, dark house. The man asks you to wait in the car while he goes ");
+    Console.WriteLine("inside to retrieve something. You notice the dome light in the car does not turn on when the car ");
+    Console.WriteLine("door opens, as if the man does not want anyone to see inside the car. The man leaves you alone, goes ");
+    Console.WriteLine("inside the house, and comes back out carrying a small brown bag.\n");
+}
+
+void room7Scenario()
+{
+    Console.WriteLine("You pull into the parking lot of an auto parts store. It is past normal business hours, ");
+    Console.WriteLine("and the store is closed. The old man makes a phone call, and a few minutes later, a loud car ");
+    Console.WriteLine("pulls up next to your car. The old man says he will just be a minute. He gets out of the car, ");
+    Console.WriteLine("goes to the loud car and gets in. A few minutes later, he returns to your car.\n");
+}
+
+void room8Scenario()
+{
+    Console.WriteLine("You drive into a nice little park. The park is closed and deserted. The old man seems relieved ");
+    Console.WriteLine("to have made it here. He tells you he would like a few moments of silence while he takes his ");
+    Console.WriteLine("pain meds. He asks you to let him know if someone pulls into the park. He then enjoys getting ");
+    Console.WriteLine("some relief to his pain.\n");
+}
+void room9Scenario()
+{
+    Console.WriteLine("The car suddenly stops in the middle of the street. You look around, wondering why the old man stopped ");
+    Console.WriteLine("at this particular location. He nods his head, and you look in the direction he is nodding towards. ");
+    Console.WriteLine("You see police cars and realize you have finally made it to the Police Station. Making it home safe and sound will is ");
+    Console.WriteLine("a possibility for you.\n");
 }
