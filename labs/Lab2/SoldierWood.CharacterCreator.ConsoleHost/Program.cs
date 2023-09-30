132b5bb9c4f0e00776aaa18d21f8ceb7a8e48f24
Chris Wood <chris.wood688@my.tccd.edu>
2023-09-30 10:09:22 -0500

    Intro 09 30 23

--- labs/Lab2/SoldierWood.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/SoldierWood.CharacterCreator.ConsoleHost/Program.cs
@@ -1,2 +1,23 @@
-﻿// Trying again
-Console.WriteLine("Hello, World!");
+﻿// ITSE 1430 Fall 2023
+// Lab 2 Character Creator
+// Written by Chris "Soldier" Wood
+// 09 30 23
+
+
+internal class Program
+{
+    private static void Main ( string[] args )
+    {
+
+        DisplayIntro();
+
+        void DisplayIntro ()
+        {
+
+            Console.WriteLine("ITSE 1430 Character Creator");
+            Console.WriteLine("Written by Chris \"Soldier\" Wood");
+            Console.WriteLine("Fall 2023\n");
+
+        }
+    }
+}
--- labs/Lab2/SoldierWood.CharacterCreator/SoldierWood.CharacterCreator/Class1.cs
+++ labs/Lab2/SoldierWood.CharacterCreator/SoldierWood.CharacterCreator/Class1.cs
@@ -1,4 +1,8 @@
-﻿namespace SoldierWood.CharacterCreator
+﻿// ITSE 1430 Fall 2023
+// Written by Chris "Soldier" Wood
+// 09 30 23
+
+namespace SoldierWood.CharacterCreator
 {
     public class Class1
     {
