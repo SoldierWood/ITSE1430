89226e8da8725b4036d82f763217d81cc79db0cf
Chris Wood <chris.wood688@my.tccd.edu>
2023-09-30 14:02:50 -0500

    Character class 09 30 23 14:02

---
+++ labs/Lab2a/SoldierCWood.CharacterCreator/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoldierCWood.CharacterCreator
+{
+    public class Character
+    {
+        public Character() { }
+
+        public string Name
+        { get; set; }
+
+    }
+
+
+}
