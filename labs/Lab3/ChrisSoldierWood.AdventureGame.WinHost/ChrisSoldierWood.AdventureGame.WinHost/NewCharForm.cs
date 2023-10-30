3c391c22b369db1f4e1db3f339d00ebb091d9b08
Chris Wood <chris.wood688@my.tccd.edu>
2023-10-30 12:00:08 -0500

    finished bio 10 30 12:00

--- labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/NewCharForm.cs
+++ labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/NewCharForm.cs
@@ -12,44 +12,52 @@ namespace ChrisSoldierWood.AdventureGame.WinHost
 {
     public partial class NewCharForm : Form
     {
-        public NewCharForm ()
+        public NewCharForm()
         {
             InitializeComponent();
         }
 
 
-        private void OnValidateName ( object sender, CancelEventArgs e )
+        private void OnValidateName(object sender, CancelEventArgs e)
         {
             if (String.IsNullOrEmpty(_txtCharName.Text))
             {
                 // Invalid
                 _error.SetError(_txtCharName, "Title is required");
                 e.Cancel = true;
-            } else
+            }
+            else
                 _error.SetError(_txtCharName, "");
 
         }
 
-        private void OnValidateProfession ( object sender, CancelEventArgs e )
+        private void OnValidateProfession(object sender, CancelEventArgs e)
         {
             if (String.IsNullOrEmpty(_cbProfession.Text))
             {
                 //Invalid
                 _error.SetError(_cbProfession, "Profession is required");
                 e.Cancel = true;
-            } else
+            }
+            else
                 _error.SetError(_cbProfession, "");
         }
 
-        private void OnValidateRace ( object sender, CancelEventArgs e )
+        private void OnValidateRace(object sender, CancelEventArgs e)
         {
             if (String.IsNullOrEmpty(_cbRace.Text))
             {
                 //Invalid
                 _error.SetError(_cbRace, "Profession is required");
                 e.Cancel = true;
-            } else
+            }
+            else
                 _error.SetError(_cbRace, "");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 
