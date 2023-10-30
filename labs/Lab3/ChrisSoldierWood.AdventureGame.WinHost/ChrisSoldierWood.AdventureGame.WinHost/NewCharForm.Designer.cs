526cbcf96698696ac7a0a428be7f230d1297298a
Chris Wood <chris.wood688@my.tccd.edu>
2023-10-30 09:48:17 -0500

    start rebuild create new char 10 30 23 9:48

--- labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/NewCharForm.Designer.cs
+++ labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/NewCharForm.Designer.cs
@@ -41,39 +41,26 @@
             this._txtName.Name = "_txtName";
             this._txtName.Size = new System.Drawing.Size(100, 23);
             this._txtName.TabIndex = 0;
-            this._txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            this._txtName.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateName);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Character Name";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
             // 
             // _error
             // 
-            this._error.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
             this._error.ContainerControl = this;
             // 
             // NewCharForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this._txtName);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            this.ClientSize = new System.Drawing.Size(610, 432);
             this.Name = "NewCharForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Create New Character";
             ((System.ComponentModel.ISupportInitialize)(this._error)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
