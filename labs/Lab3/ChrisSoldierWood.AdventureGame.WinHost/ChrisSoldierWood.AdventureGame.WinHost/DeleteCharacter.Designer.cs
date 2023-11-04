be089eee8052114951d74c3e226a0310351d2309
Chris Wood <chris.wood688@my.tccd.edu>
2023-11-04 10:31:46 -0500

    fixed exit form closing AGAIN 11 04 10:31

--- labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/DeleteCharacter.Designer.cs
+++ labs/Lab3/ChrisSoldierWood.AdventureGame.WinHost/ChrisSoldierWood.AdventureGame.WinHost/DeleteCharacter.Designer.cs
@@ -28,28 +28,28 @@
         /// </summary>
         private void InitializeComponent ()
         {
-            button1=new Button();
-            SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // button1
             // 
-            button1.Location=new Point(218, 273);
-            button1.Name="button1";
-            button1.Size=new Size(75, 23);
-            button1.TabIndex=0;
-            button1.Text="Yes";
-            button1.UseVisualStyleBackColor=true;
-            button1.Click+=button1_Click;
+            this.button1.Location = new System.Drawing.Point(212, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yes";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // DeleteCharacter
             // 
-            AutoScaleDimensions=new SizeF(7F, 15F);
-            AutoScaleMode=AutoScaleMode.Font;
-            ClientSize=new Size(800, 450);
-            Controls.Add(button1);
-            Name="DeleteCharacter";
-            Text="DeleteCharacter";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "DeleteCharacter";
+            this.Text = "DeleteCharacter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
