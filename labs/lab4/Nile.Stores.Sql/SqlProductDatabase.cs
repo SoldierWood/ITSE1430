e3d6f4804094411171d90d5cfe39702534340bbe
Chris Wood <chris.wood688@my.tccd.edu>
2023-12-01 08:27:55 -0600

    Started story 6 added SqlProductDatabase.cs 12-1 8:27

---
+++ labs/lab4/Nile.Stores.Sql/SqlProductDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace Nile.Stores.Sql
+{
+    public class SqlProductDatabase : ProductDatabase
+    {
+        protected override Product GetCore ( int id );
+
+        protected override IEnumerable<Product> GetAllCore ();
+
+        protected override void RemoveCore ( int id );
+
+        protected override Product UpdateCore ( Product existing, Product newItem );
+
+        protected override Product AddCore ( Product product );
+
+    }
+}
